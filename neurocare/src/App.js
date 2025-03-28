8d451c47b79a57493947004af207a74848d00e18
nonotor67 <contal.noah@gmail.com>
2025-03-28 23:05:36 +0100

    feat: init with react

---
+++ neurocare/src/App.js
@@ -0,0 +1,17 @@
+import logo from './pp.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="NeuroCare">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+            NeuroCare
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
